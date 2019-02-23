868ca450e9a13c81cc29b4d6c6702fa5cd2d495e
Manikonda <Manikonda.M@igglobal.com>
2019-02-23 14:25:41 +0530

    adding the observable epic and the redux functionality for observable

--- src/App.js
+++ src/App.js
@@ -1,28 +1,37 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 import './App.css';
+
+import { fetchWhiskies } from './actions';
+
+import WhiskyGrid from './components/WhiskyGrid';
 
 class App extends Component {
   render() {
+    const {
+      fetchWhiskies,
+      isLoading,
+      error,
+      whiskies
+    } = this.props;
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <button onClick={fetchWhiskies} style={{color:'white',cursor:'pointer',fontSize:'1.5rem',borderRadius:'0.8rem',border:'0.2rem solid blue',backgroundColor:'green',padding:'0.5rem',margin:'1rem'}}>Fetch whiskies</button>
+        {isLoading && <h1>Fetching data</h1>}
+        {!isLoading && !error && <WhiskyGrid whiskies={whiskies} />}
+        {error && <h1>{error}</h1>}
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = state => ({ ...state });
+
+const mapDispatchToProps = dispatch =>
+    bindActionCreators({
+        fetchWhiskies
+    }, dispatch);
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -2,9 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import { createEpicMiddleware } from "redux-observable";
 import * as serviceWorker from './serviceWorker';
+import rootReducer from "./reducers/root";
+import { rootEpic } from './epics';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const epicMiddleware = createEpicMiddleware();
+
+const store = createStore(rootReducer, applyMiddleware(epicMiddleware));
+epicMiddleware.run(rootEpic);
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
